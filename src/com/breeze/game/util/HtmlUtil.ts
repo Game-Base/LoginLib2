3ec1dfcb95f827398804796710329821459bac46
wangyuan0810@163.com <wangyuan0810@163.com>
2020-10-10 20:54:03 +0800

    1

--- src/com/breeze/game/util/HtmlUtil.ts
+++ src/com/breeze/game/util/HtmlUtil.ts
@@ -110,8 +110,29 @@ module qmr
         }
 
         public static isPhoneNumber(phoneNum): boolean {
-            let reg = /^1(3[0-9]|4[5,7]|5[0,1,2,3,5,6,7,8,9]|6[2,5,6,7]|7[0,1,7,8]|8[0-9]|9[1,8,9])\d{8}$/;
-            return reg.test(phoneNum);
+            // let reg = /^1(3[0-9]|4[5,7]|5[0,1,2,3,5,6,7,8,9]|6[2,5,6,7]|7[0,1,7,8]|8[0-9]|9[1,8,9])\d{8}$/;
+			// return reg.test(phoneNum);
+			if(!phoneNum){
+				return false;
+			}
+			if(!HtmlUtil.IsInteger(phoneNum)){
+				return;
+			}
+			return String(phoneNum).length == 11;
+		}
+		
+		/**
+         * 判断输入的字符是否为整数
+         */
+        public static IsInteger(value):boolean {
+            var str = value.trim();
+            if (str.length != 0) {
+                let reg =new RegExp(/^[-+]?\d*$/);
+                if (!reg.test(str)) {
+                    return false;
+                }
+            }
+            return true;
         }
 
 	}
